44624db019a8526e8e080708fed78d85a596a4b7
amakasu <circularimage@gmail.com>
2016-05-26 12:57:23 +0900

    fix base dir

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 
 gulp.task('lint', () => {
-  gulp.src('**/*.+(js|jsx)', { base: ['client', 'server'] })
+  gulp.src('**/*.+(js|jsx)', { base: ['src'] })
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
