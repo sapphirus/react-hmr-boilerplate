1c8249df802216635c8bb0f53cd9f75a59928d03
amakasu <circularimage@gmail.com>
2016-06-07 13:08:34 +0900

    Fix gulp.config.js

--- gulp.config.js
+++ gulp.config.js
@@ -3,35 +3,33 @@ import webpackConfig from './webpack.config.babel';
 const join = path.join;
 // const relative = path.relative;
 const base = join(__dirname, 'src');
-const client = `${base}/client`;
 const assets = `${base}/assets`;
 const dist = `${base}/dist`;
 const build = `${base}/build`;
 
 const config = {
-  dest: dist,
   clean: {
     src: [dist, build],
   },
   css: {
-    base: `${dist}`,
+    base: dist,
     src: ['**/*.css', build],
   },
   assets: {
-    base: `${assets}`,
+    base: assets,
     src: [dist, build],
   },
   js: {
-    base: `${dist}`,
-    src: ['**/*.+js', build],
+    base: dist,
+    src: ['**/*.js', build],
   },
   lint: {
     base: `${base}`,
     src: '**/*.+(js|jsx)',
   },
   webpack: {
-    base: `${client}`,
-    dest: `${dist}`,
+    base: `${base}/client`,
+    dest: dist,
     options: webpackConfig,
   },
 };
